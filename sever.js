fbaf3c57a730cf0ee7f9cc0deca274d895e1755d
kquan2312 <95995990+kquan2312@users.noreply.github.com>
2025-02-21 11:00:56 +0700

    create git ignore

--- sever.js
+++ sever.js
@@ -4,10 +4,10 @@ const server = http.createServer((req, res) => {
   console.log("run request ...");
   res.setHeader("Content-Type", "text/html");
   res.write("<h3>Hello world!</h3>");
-  res.write("<h2>Học lập trình web với nodejs</h2>");
+  res.write("<h2>Learn Nodejs on ytube</h2>");
   res.end();
 });
 
-server.listen(3000, "localhost", () => {
-  console.log("Node.JS server is running on port: 3000");
+server.listen(4000, "localhost", () => {
+  console.log("Node.JS server is running on port: 4000");
 });
